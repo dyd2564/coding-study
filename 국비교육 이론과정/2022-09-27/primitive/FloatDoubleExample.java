4c71c46abcc63ec54860889be7ffc4b6a2c81740
dyd2564 <108705911+dyd2564@users.noreply.github.com>
2022-09-27 15:41:09 +0900

    Add files via upload

---
+++ 국비교육 이론과정/2022-09-27/primitive/FloatDoubleExample.java
@@ -0,0 +1,21 @@
+package primitive;
+
+public class FloatDoubleExample {
+	public static void main(String[] args) {
+		double var1 = 0.1234567890123456789;
+		float var2 = 0.1234567890123456789f;
+		System.out.println(var1);
+		System.out.println(var2);
+		
+		double var3 = 3e6;
+		double var4 = 3e-6;
+		float var5 = 3e6f;
+		float var6 = 3e-6f;
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+		
+		
+	}
+}
