f7a948429437bfd469dcc09e806e133dc12bae85
dyd2564 <108705911+dyd2564@users.noreply.github.com>
2022-11-08 16:14:02 +0900

    Add files via upload

---
+++ 국비교육 이론과정/2022-11-08/ex01/GetServletContext02.java
@@ -0,0 +1,32 @@
+package sec05.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cget2")
+public class GetServletContext02 extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		
+		// 서블릿 컨텍스트 얻기
+		ServletContext ctx = getServletContext();
+		List<String> memberList = (List<String>) ctx.getAttribute("members");
+		String member1 = memberList.get(0);
+		String member2 = memberList.get(1);
+		out.print("cget2<br>");
+		out.print(member1+"<br>");
+		out.print(member2+"<br>");
+		
+	}
+}
