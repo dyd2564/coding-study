9c8dbebeba8c730fd68f4916a9598c06374050a0
dyd2564 <108705911+dyd2564@users.noreply.github.com>
2022-11-25 15:58:30 +0900

    Add files via upload

---
+++ 국비교육 이론과정/2022-11-25/domain/BoardVO.java
@@ -0,0 +1,28 @@
+package com.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class BoardVO {
+
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date writeDate;
+
+	
+	
+}
