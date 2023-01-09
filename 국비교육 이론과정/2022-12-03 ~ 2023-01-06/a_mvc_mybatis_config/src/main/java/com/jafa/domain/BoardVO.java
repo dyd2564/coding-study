f681b9cec053843e84efc158e0e20de94612f56b
dyd2564 <108705911+dyd2564@users.noreply.github.com>
2023-01-09 09:25:41 +0900

    Add files via upload

---
+++ 국비교육 이론과정/2022-12-03 ~ 2023-01-06/a_mvc_mybatis_config/src/main/java/com/jafa/domain/BoardVO.java
@@ -0,0 +1,18 @@
+package com.jafa.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardVO {
+	String title = "아햏햏";
+	String contetn = "easy";
+	String writer = "FFFFFFFDDDD";
+}
